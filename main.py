345b578e9f14b3047c2e616db559790f6784e479
AStrzebonski <46843487+AStrzebonski@users.noreply.github.com>
2022-09-03 23:31:52 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 import string
 
 def get_random_string(length):
-    # choose from all lowercase letter
+    # choose from all letters
     letters = string.ascii_letters
     result_str = ''.join(random.choice(letters) for i in range(length))
     return result_str
